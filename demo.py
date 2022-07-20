eca81086ae8845e0944d04068169547b9b9fb0e1
ILAYA RAJA <98961149+Mr-Machine07@users.noreply.github.com>
2022-07-20 13:15:22 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,18 @@
+from market import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
+def dummy():
+    def market_page():
+        items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+        ]
